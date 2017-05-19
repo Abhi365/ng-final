0679b5b55587cbb29fbcbcc6399d2c538f06e7a2
Bansal <abhishek.b.bansal@capgemini.com>
2017-05-19 17:43:37 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
+import { PostsComponent } from './posts/posts.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { Routing } from './routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserComponent,
+    PostsComponent,
+    NotfoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
